0c2c6c4f70afc912c5fc07fe09f219b8d4736feb
Eric Bischoff <erichoff4050@gmail.com>
2019-03-26 12:48:39 -0500

    configuring for heroku

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+"""mysite URL Configuration
+...
+"""
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
